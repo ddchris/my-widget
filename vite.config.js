c248839d0304d34eef363d8aaed6b4b4086108af
ddchris <evilkillerice@gmail.com>
2025-10-31 19:10:51 +0800

    C

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     rollupOptions: {
       // 不特別 externalize，讓 Vue 一併被打包到輸出檔（簡單做法）
       // 如果你想外部化 Vue（不把 Vue 包進去），可把 'vue' 加到 external 並再改消費方用法（見下）
-      external: ['vue']
+      external: []
     }
   }
 })
