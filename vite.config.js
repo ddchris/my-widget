836a268248ce8a85a4c15b17dbf4c43c053c9b93
ddchris <evilkillerice@gmail.com>
2025-11-01 11:48:41 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+
 export default defineConfig({
-  plugins: [vue({ customElement: true })], // 若要打包成 Web Component 建議加這個
+  plugins: [vue({ customElement: true })], // 打包成 Web Component
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/entry.js'),
-      formats: ['es'],
-      fileName: () => 'base-button.es.js',
+      formats: ['iife'],
+      name: 'BaseButton', // 全域變數名稱
     },
     rollupOptions: {
-      // ⛔ 不要打包 vue、element-plus、icons-vue
-      external: ['vue', 'element-plus', '@element-plus/icons-vue'],
+      external: ['vue', 'element-plus'],
       output: {
-        inlineDynamicImports: true, // 確保單檔輸出
         globals: {
           vue: 'Vue',
           'element-plus': 'ElementPlus',
-          '@element-plus/icons-vue': 'ElementPlusIconsVue',
         },
       },
     },
