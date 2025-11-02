b63b51cf9d4200858446fac34b765b460c30274f
ddchris <evilkillerice@gmail.com>
2025-11-02 13:22:46 +0800

    .

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -19,17 +19,6 @@ const emit = defineEmits(['click'])
 const loadingBool = computed(() => props.loading === true || props.loading === 'true')
 
 // ✅ 自動解析 icon
-const resolvedIcon = computed(() => {
-  // 若是字串，從 ElementPlusIconsVue 中查找
-  if (typeof props.icon === 'string') {
-    return iconMap[props.icon] || null
-  }
-  // 若是物件 (例如直接傳入 Edit component)
-  if (typeof props.icon === 'object' || typeof props.icon === 'function') {
-    return props.icon
-  }
-  return null
-})
 </script>
 
 <template>
@@ -43,8 +32,8 @@ const resolvedIcon = computed(() => {
     <ElIcon v-if="loadingBool"><Loading class="animate-spin" /></ElIcon>
 
     <!-- 有 icon -->
-    <ElIcon v-else-if="resolvedIcon">
-      <component :is="resolvedIcon" />
+    <ElIcon v-else-if="props.icon">
+      <component :is="props.icon" />
     </ElIcon>
 
     <!-- 顯示名稱 -->
